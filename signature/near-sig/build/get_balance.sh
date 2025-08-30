a140d67077b52aed8f02d769ae59738c3896525b
ayushsingh82 <ayushsinghmi711@gmail.com>
2025-08-31 02:24:53 +0530

    refine readme

--- signature/near-sig/build/get_balance.sh
+++ signature/near-sig/build/get_balance.sh
@@ -4,7 +4,7 @@
 
 ACCOUNT="nexchangenear.near"
 
-CONTRACTS=( "sol.omft.near" "aptos-88cb7619440a914fe6400149a12b443c3ac21d59.omft.near" "duelx.near"  "ray.tkn.near"   "poop.token0.near" "gtf.aidols.near" "cos.tkn.near" "musk.token0.near" "morexrp.mmroot.near" "ccc-5.aidols.near" "kmdrqa.ribano.near" "nbtc.bridge.near" "token.sweat"   "eth.bridge.near"   "aaaaaa20d9e0e2461697782ef11675f668207961.factory.bridge.near"  "853d955acef822db058eb8505911ed77f175b99e.factory.bridge.near"   "2260fac5e5542a773aa44fbcfedf7c193bc2c599.factory.bridge.near")
+CONTRACTS=( "sol.omft.near" "aptos-88cb7619440a914fe6400149a12b443c3ac21d59.omft.near" "duelx.near"  "ray.tkn.near"   "poop.token0.near" "gtf.aidols.near" "cos.tkn.near" "musk.token0.near" "morexrp.mmroot.near" "ccc-5.aidols.near" "kmdrqa.ribano.near" "nbtc.bridge.near" "token.sweat"   "eth.bridge.near"   "aaaaaa20d9e0e2461697782ef11675f668207961.factory.bridge.near"  "853d955acef822db058eb8505911ed77f175b99e.factory.bridge.near"   "2260fac5e5542a773aa44fbcfedf7c193bc2c599.factory.bridge.near"   "853d955acef822db058eb8505911ed77f175b99e.factory.bridge.near"   "2260fac5e5542a773aa44fbcfedf7c193bc2c599.factory.bridge.near"  "token.rhealab.near"  "token.publicailab.near" "jambo-1679.meme-cooking.near" "xrp.omft.near" "doge.omft.near" )
 
 
 for c in "${CONTRACTS[@]}"
